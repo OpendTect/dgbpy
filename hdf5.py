5d66dec79aa5a4b2ee70b93e0784fbd09ba6a909
arnauddgbes <arnaud.huck@dgbes.com>
2018-11-26 11:52:19 +0100

    dgb: added functions to dgbhdf5 for parsing both types of training datasets headers

--- hdf5.py
+++ hdf5.py
@@ -7,61 +7,119 @@
 #
 
 
-import collections
 import numpy as np
 import h5py
 import odpy.hdf5 as odhdf5
 
+def validInfo( info ):
+  try:
+    type = odhdf5.getText(info,"Type")
+  except KeyError:
+    print("No type found. Probably wrong type of hdf5 file")
+    return False
+  return True
+
+def getBaseInfo( info, reqtype ):
+  if not validInfo( info ):
+    return {}
+
+  type = odhdf5.getText(info,'Type')
+  if type != reqtype:
+    print( "Wrong type of training dataset. Should be: "+reqtype )
+    raise KeyError
+
+  stepout = odhdf5.getIStepInterval(info,"Stepout") 
+  ex_sz = odhdf5.getIntValue(info,"Examples.Size") 
+  idx = 0
+  examples = list()
+  while idx < ex_sz:
+    namestr = "Examples."+str(idx)+".Name"
+    logstr = "Examples."+str(idx)+".Log"
+    if odhdf5.hasAttr( info, namestr ):
+      exname = namestr
+      extype = "Point-Set"
+    elif odhdf5.hasAttr( info, logstr ):
+      exname = logstr
+      extype = "Log"
+    else:
+      raise KeyError
+    example_sz = odhdf5.getIntValue(info,"Examples."+str(idx)+".Size")
+    example_id = list()
+    idy = 0
+    while idy < example_sz:
+      example_id.append(odhdf5.getText(info,"Examples."+str(idx)+".ID."+str(idy)))
+      idy += 1
+    example = {
+      "name": odhdf5.getText( info, exname ),
+      "type": extype,
+      "id": example_id
+    }
+    surveystr = "Examples."+str(idx)+".Survey"
+    if odhdf5.hasAttr( info, surveystr ):
+      surveynm = odhdf5.getText(info, surveystr )
+      example.update({'Survey': odhdf5.getText(info, surveystr )})
+
+    examples.append( example )
+    idx += 1
+
+  inp_sz = odhdf5.getIntValue(info,"Input.Size")
+  idx = 0
+  input = list()
+  while idx < inp_sz:
+    inp = {
+      "survey": odhdf5.getText(info,"Input."+str(idx)+".Survey")
+    }
+    logsstr = "Input."+str(idx)+".Logs"
+    if odhdf5.hasAttr( info, logsstr ):
+      inp.update({'Logs': odhdf5.getText(info, logsstr )})
+    inpsizestr = "Input."+str(idx)+".Size"
+    if odhdf5.hasAttr( info, inpsizestr ):
+      inp_id = list()
+      idy = 0
+      inpp_sz = odhdf5.getIntValue(info,inpsizestr)
+      while idy < inpp_sz:
+        inp_id.append(odhdf5.getText(info,"Input."+str(idx)+".ID."+str(idy)))
+        idy += 1
+      inp.update({'id': inp_id})
+
+    input.append( inp )
+    idx += 1
+
+  return {
+    'type': type,
+    'stepout': stepout,
+    'interpolated': odhdf5.getBoolValue(info,"Edge extrapolation"),
+    'examples': examples,
+    'input': input
+  }
+
+def getAttribInfo( filenm ):
+  h5file = h5py.File( filenm, "r" )
+  infods = odhdf5.getInfoDataSet( h5file )
+  baseinfo = getBaseInfo( infods, 'Seismic Classification' )
+  nrsurveys = odhdf5.getIntValue( infods, 'Number of Surveys' )
+  survlist = list()
+  idx = 0
+  while idx < nrsurveys:
+    survlist.append( odhdf5.getText(infods,"Survey."+str(idx)) )
+    idx += 1
+
+  h5file.close()
+  return {
+    'base': baseinfo,
+    'surveys': survlist
+  }
 
 def getWellInfo( filenm ):
   h5file = h5py.File( filenm, "r" )
   infods = odhdf5.getInfoDataSet( h5file )
-  try:
-    type = odhdf5.getText(infods,"Type")
-  except KeyError:
-    print("No type found. Probably wrong type of hdf5 file")
-    return {}
-
-  ex_sz = odhdf5.getIntValue(infods,"Examples.Size") 
-  idx = 0
-  examples = list()
-  while idx < ex_sz:
-    example_sz = odhdf5.getIntValue(infods,"Examples."+str(idx)+".Size")
-    example_id = list()
-    idy = 0
-    while idy < example_sz:
-      example_id.append(odhdf5.getText(infods,"Examples."+str(idx)+".ID."+str(idy)))
-      idy += 1
-    example = {
-      "name": odhdf5.getText(infods,"Examples."+str(idx)+".Log"),
-      "id": example_id,
-      "survey": odhdf5.getText(infods,"Examples."+str(idx)+".Survey")
-    }
-    examples.append( example )
-    idx += 1
-
-  inp_sz = odhdf5.getIntValue(infods,"Input.Size")
-  idx = 0
-  input = list()
-  while idx < inp_sz:
-    inp = collections.OrderedDict({
-      "name": odhdf5.getText(infods,"Input."+str(idx)+".Logs"),
-      "survey": odhdf5.getText(infods,"Input."+str(idx)+".Survey")
-    })
-    input.append( inp )
-    idx += 1
-
+  baseinfo = getBaseInfo( infods, 'Log-Log Prediction' )
   zstep = odhdf5.getDValue(infods,"Z step") 
-  stepout = odhdf5.getIntValue(infods,"Stepout") 
   marker = (odhdf5.getText(infods,"Top marker"),
             odhdf5.getText(infods,"Bottom marker"))
-  isinterpol = odhdf5.getBoolValue(infods,"Edge extrapolation")
   h5file.close()
-  return collections.OrderedDict({
-    'examples': examples,
-    'input': input,
+  return {
+    'base': baseinfo,
     'zstep': zstep,
-    'stepout': stepout,
-    'marker': marker,
-    'interpolated': isinterpol
-  })
+    'range': marker,
+  }
