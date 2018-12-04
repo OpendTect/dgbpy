92e4c4576c3a90b4db1963dfb8804aa077f89c09
arnauddgbes <arnaud.huck@dgbes.com>
2018-12-04 17:04:47 +0100

    dgb: further work on Keras PyQt gui

--- keras_gui.py
+++ keras_gui.py
@@ -10,14 +10,30 @@
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QCheckBox, QDialog, QDialogButtonBox,
-                             QFrame, QFormLayout,QLabel, QSpinBox, QVBoxLayout,
-                             QWidget)
+                             QFileDialog,
+                             QFrame, QFormLayout, QHBoxLayout, QLineEdit,
+                             QPushButton, QSizePolicy,
+                             QSpinBox, QVBoxLayout, QWidget)
 
 def getSpinBox(min,max,defval):
   ret = QSpinBox()
   ret.setRange(min,max)
   ret.setValue(defval)
+  ret.setSizePolicy( QSizePolicy.Fixed, QSizePolicy.Preferred )
   return ret
+
+def getFileInput(filenm):
+  ret = QHBoxLayout()
+  inpfilefld = QLineEdit(filenm)
+  inpfilefld.setMinimumWidth(200)
+  fileselbut = QPushButton('Select')
+  ret.addWidget(inpfilefld)
+  ret.addWidget(fileselbut)
+  return (ret, inpfilefld, fileselbut)
+
+def selectInput(parent,dlgtitle,dirnm,filters,lineoutfld):
+  newfilenm = QFileDialog.getOpenFileName(parent,dlgtitle,dirnm,filters)
+  lineoutfld.setText( newfilenm[0] )
 
 def addSeparator(layout):
   linesep = QFrame()
@@ -29,6 +45,7 @@ class WidgetGallery(QDialog):
   def __init__(self, args, parent=None):
     super(WidgetGallery, self).__init__(parent)
     self.lm = odcommon.LogManager( args )
+    self.h5filenm = args['h5file'].name
 
     mainform = QFormLayout()
     mainform.setLabelAlignment( Qt.AlignRight )
@@ -44,11 +61,22 @@ class WidgetGallery(QDialog):
     self.setLayout( mainlayout )
 
   def createInputGroupBox(self,layout):
-    self.inputfld = QLabel('path-to-h5file.h5')
-    self.inplogfld = QLabel('path-to-logfile.txt')
-
+    (self.inputfld, self.filenmfld, self.fileselbut) = getFileInput( self.h5filenm )
     layout.addRow( "&Train data", self.inputfld )
-    layout.addRow( "&Log File", self.inplogfld )
+    layout.labelForField(self.inputfld).setBuddy(self.fileselbut)
+    self.fileselbut.clicked.connect(lambda: selectInput(self,"Select training dataset",
+                                                os.path.dirname( self.filenmfld.text() ),
+                                                "HDF5 Files (*.hdf5 *.h5)",
+                                                self.filenmfld) )
+
+    if self.lm.log_file.name != '<stdout>':
+      (self.logfld, self.lognmfld, self.logselbut) = getFileInput( self.lm.log_file.name )
+      layout.addRow( "&Log File", self.logfld )
+      layout.labelForField(self.logfld).setBuddy(self.logselbut)
+      self.logselbut.clicked.connect(lambda: selectInput(self,"Select Log File",
+                                                os.path.dirname( self.lm.log_file.name ),
+                                                "Log Files (*.txt *.TXT *.log)",
+                                                self.lognmfld) )
 
   def createParametersGroupBox(self,layout):
     self.iterfld = getSpinBox(1,100,15)
@@ -70,8 +98,8 @@ class WidgetGallery(QDialog):
     layout.addRow( "&Patience", self.patiencefld )
 
   def createOutputGroupBox(self,layout):
-    self.modelfld = QLabel("Trained HDF5 model")
-    layout.addRow( "&Trained model", self.modelfld )
+    self.modelfld = QLineEdit("<new model>")
+    layout.addRow( "&Output model", self.modelfld )
 
   def createButtonsBox(self):
     buttons = QDialogButtonBox()
@@ -84,10 +112,7 @@ class WidgetGallery(QDialog):
     self.buttonslayout = QVBoxLayout()
     self.buttonslayout.addWidget( buttons )
 
-  def doApply(self):
-    self.printSummary();
-
-  def printSummary(self):
+  def getParams(self):
     ret = {
       'num_tot_iterations': self.iterfld.value(),
       'epochs': self.epochfld.value(),
@@ -96,7 +121,15 @@ class WidgetGallery(QDialog):
     }
     if self.dodecimate.isChecked():
       ret.update({'num_train_ex': self.decimatefld.value()})
-    self.lm.log_msg( ret )
+    return ret
+
+  def doApply(self):
+    params = self.getParams();
+    self.lm.log_msg( "Input: " + os.path.basename(self.filenmfld.text()) )
+    if self.lognmfld != None:
+      self.lm.log_msg( "Log: " + os.path.basename(self.lognmfld.text()) )
+    self.lm.log_msg( params )
+
 
 def setStyleSheet( app ):
   cssfile = open( "/auto/d29/arnaud/dev/od/data/Styles/default.qss", "r" )
@@ -107,24 +140,28 @@ def setStyleSheet( app ):
 
 if __name__ == '__main__':
 
-    import os
-    import signal
     import sys
+    import os
     import argparse
+    import signal
     import odpy.common as odcommon
 
     parser = argparse.ArgumentParser(prog='PROG',description='Select parameters for training a Keras model')
     parser.add_argument('-v','--version',action='version',version='%(prog)s 1.0')
-    parser.add_argument('--log',dest='logfile',metavar='file',nargs='?',type=argparse.FileType('a'),
+    parser.add_argument('h5file',type=argparse.FileType('r'),
+                        help='HDF5 file containing the training data')
+    parser.add_argument('--log',dest='logfile',metavar='file',nargs='?',
+                        type=argparse.FileType('a'),
                         default='sys.stdout',help='Progress report output')
-    parser.add_argument('--syslog',dest='sysout',metavar='stdout',nargs='?',type=argparse.FileType('a'),
+    parser.add_argument('--syslog',dest='sysout',metavar='stdout',nargs='?',
+                        type=argparse.FileType('a'),
                         default='sys.stdout',help='Standard output')
     args = vars(parser.parse_args())
 
     app = QApplication(sys.argv)
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
     gallery = WidgetGallery(args)
     gallery.show()
     setStyleSheet( app )
 
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
     sys.exit(app.exec_()) 
