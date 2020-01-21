a4a57f1d2dcf0c4e1f46ecdef865023ee4dfae13
waynegm <waynegm@users.noreply.github.com>
2020-01-21 14:19:14 +1000

    dgb: Add communication from bokeh_machine_learning to deeplearningtrainingmgr Fixes OD-2514 and OD-2503
    Summary:
    Adds a basic client to the bokeh_machine_learning main.py script to send some basic information
    such as 'training started', 'training finished' and 'platform changed' back to OpendTect.
    Corresponding callbacks/actions implemented in deeplearningtraningmgr.cc.
    Fixes OD-2514 OD-2503
    
    Test Plan: Working on Linux
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Differential Revision: https://phabricator.dgbes.com:8443/D13133

--- bokeh_machine_learning/main.py
+++ bokeh_machine_learning/main.py
@@ -7,22 +7,7 @@
 # _________________________________________________________________________
 
 import sys
-from os import path
 import argparse
-import psutil
-from functools import partial
-
-from bokeh.io import curdoc
-from bokeh.layouts import column
-from bokeh.models.widgets import Panel, Select, Tabs, TextInput
-
-import odpy.common as odcommon
-from odpy.oscommand import (getPythonCommand, execCommand, kill,
-                            isRunning, pauseProcess, resumeProcess)
-import dgbpy.keystr as dgbkeys
-from dgbpy import mlapply as dgbmlapply
-from dgbpy import uibokeh, uikeras, uisklearn
-from dgbpy import mlio as dgbmlio
 
 parser = argparse.ArgumentParser(
             description='Select parameters for machine learning model training')
@@ -31,6 +16,23 @@ parser.add_argument( '-v', '--version',
 parser.add_argument( 'h5file',
             type=argparse.FileType('r'),
             help='HDF5 file containing the training data' )
+netgrp = parser.add_argument_group( 'Network' )
+netgrp.add_argument( '--address',
+            dest='addr', metavar='ADDRESS', action='store',
+            type=str, default='localhost',
+            help='Address to listen on' )
+netgrp.add_argument( '--port',
+            dest='port', action='store',
+            type=int, default=65432,
+            help='Port to listen on')
+netgrp.add_argument( '--dlserver',
+            dest='dlserver', action='store',
+            type=str, default='',
+            help='Deep Learning service manager connection details')
+netgrp.add_argument( '--ppid',
+            dest='ppid', action='store',
+            type=int, default=-1,
+            help='PID of the parent process' )
 datagrp = parser.add_argument_group( 'Data' )
 datagrp.add_argument( '--dataroot',
             dest='dtectdata', metavar='DIR', nargs=1,
@@ -66,164 +68,193 @@ loggrp.add_argument( '--syslog',
             type=argparse.FileType('a'), default=sys.stdout,
             help='Standard output' )
 args = vars(parser.parse_args())
+
+import odpy.common as odcommon
 odcommon.initLogging( args )
 odcommon.proclog_logger.setLevel( 'DEBUG' )
 
-examplefilenm =  args['h5file'].name
-trainscriptfp = path.join(path.dirname(path.dirname(__file__)),'mlapplyrun.py')
+import dgbpy.servicemgr as dgbservmgr
 
-traintabnm = 'Training'
-paramtabnm = 'Parameters'
+with dgbservmgr.ServiceMgr(args['dlserver'], args['ppid']) as this_service:
 
-trainpanel = Panel(title=traintabnm)
-parameterspanel = Panel(title=paramtabnm)
-mainpanel = Tabs(tabs=[trainpanel,parameterspanel])
+# Keep all lengthy operations below
+  from os import path
+  import psutil
+  from functools import partial
 
-ML_PLFS = []
-ML_PLFS.append( uikeras.getPlatformNm(True) )
-ML_PLFS.append( uisklearn.getPlatformNm(True) )
+  from bokeh.io import curdoc
+  from bokeh.layouts import column
+  from bokeh.models.widgets import Panel, Select, Tabs, TextInput
 
-platformfld = Select(title="Machine learning platform:",options=ML_PLFS)
-platformparsbut = uibokeh.getButton(paramtabnm,\
+  from odpy.oscommand import (getPythonCommand, execCommand, kill,
+                            isRunning, pauseProcess, resumeProcess)
+  import dgbpy.keystr as dgbkeys
+  from dgbpy import mlapply as dgbmlapply
+  from dgbpy import uibokeh, uikeras, uisklearn
+  from dgbpy import mlio as dgbmlio
+
+  examplefilenm =  args['h5file'].name
+  trainscriptfp = path.join(path.dirname(path.dirname(__file__)),'mlapplyrun.py')
+
+  traintabnm = 'Training'
+  paramtabnm = 'Parameters'
+
+  trainpanel = Panel(title=traintabnm)
+  parameterspanel = Panel(title=paramtabnm)
+  mainpanel = Tabs(tabs=[trainpanel,parameterspanel])
+
+  ML_PLFS = []
+  ML_PLFS.append( uikeras.getPlatformNm(True) )
+  ML_PLFS.append( uisklearn.getPlatformNm(True) )
+
+  platformfld = Select(title="Machine learning platform:",options=ML_PLFS)
+  platformparsbut = uibokeh.getButton(paramtabnm,\
     callback_fn=partial(uibokeh.setTabFromButton,panelnm=mainpanel,tabnm=paramtabnm))
-outputnmfld = TextInput(title='Output model:')
+  outputnmfld = TextInput(title='Output model:')
 
-info = dgbmlio.getInfo( examplefilenm )
-keraspars = uikeras.getUiPars( info[dgbkeys.learntypedictstr],
+  info = dgbmlio.getInfo( examplefilenm )
+  keraspars = uikeras.getUiPars( info[dgbkeys.learntypedictstr],
                               estimatedszgb=info[dgbkeys.estimatedsizedictstr] )
-sklearnpars = uisklearn.getUiPars( info[dgbkeys.classdictstr] )
-parsgroups = (keraspars,sklearnpars)
-parsbackbut = uibokeh.getButton('Back',\
+  sklearnpars = uisklearn.getUiPars( info[dgbkeys.classdictstr] )
+  parsgroups = (keraspars,sklearnpars)
+  parsbackbut = uibokeh.getButton('Back',\
     callback_fn=partial(uibokeh.setTabFromButton,panelnm=mainpanel,tabnm=traintabnm))
 
-def mlchgCB( attrnm, old, new):
-  selParsGrp( new )
+  def mlchgCB( attrnm, old, new):
+    selParsGrp( new )
 
-def nameChgCB( attrnm, old, new):
-  if len(new) < 1:
-    return
+  def nameChgCB( attrnm, old, new):
+    if len(new) < 1:
+      return
 
-  modtype = dgbmlio.getModelType( info ) 
-  curbg = outputnmfld.background
-  (exists,sametrl,sameformat,sametyp) = \
+    modtype = dgbmlio.getModelType( info ) 
+    curbg = outputnmfld.background
+    (exists,sametrl,sameformat,sametyp) = \
                   dgbmlio.modelNameExists( new,modtype, \
                   args=args,reload=False)
-  if dgbmlio.modelNameIsFree(new,modtype,args=args,reload=False):
-    if exists and sametrl and sameformat and sametyp != None and sametyp:
-      outputnmfld.background = '#FFFF00'
-    else:
-      outputnmfld.background = None
-  else:
-    outputnmfld.background = '#FF0000'
-  outputnmfld.value = new
-  if outputnmfld.background == curbg:
-    return
-  curdoc().clear()
-  curdoc().add_root(mainpanel)
-
-def getParsGrp( platformnm ):
-  for platform,parsgroup in zip(ML_PLFS,parsgroups):
-    if platform[0] == platformnm:
-      return parsgroup['grp']
-  return None
-
-def selParsGrp( platformnm ):
-  parsgrp = getParsGrp( platformnm )
-  if parsgrp == None:
-    return
-  curdoc().clear()
-  parameterspanel.child = column( parsgrp, parsbackbut )
-  curdoc().add_root(mainpanel)
-
-def getUiParams():
-  parsgrp = getParsGrp( platformfld.value )
-  if platformfld.value == uikeras.getPlatformNm():
-    return uikeras.getUiParams( keraspars )
-  elif platformfld.value == uisklearn.getPlatformNm():
-    return uisklearn.getUiParams( sklearnpars )
-  return {}
-
-def getProcArgs( platfmnm, pars, outnm ):
-  traintype = dgbmlapply.TrainType.New
-  ret = {
-    'posargs': [examplefilenm],
-    'odargs': odcommon.getODArgs( args ),
-    'dict': {
-      'platform': platfmnm,
-      'parameters': pars,
-      'output': outnm
-    }
-  }
-  dict = ret['dict']
-  if 'model' in args:
-    model = args['model']
-    if model != None and len(model)>0:
-      dict.update({'model': model[0]})
-      if args['transfer']:
-        traintype = dgbmlapply.TrainType.Transfer
+    if dgbmlio.modelNameIsFree(new,modtype,args=args,reload=False):
+      if exists and sametrl and sameformat and sametyp != None and sametyp:
+        outputnmfld.background = '#FFFF00'
       else:
-        traintype = dgbmlapply.TrainType.Resume
-  if 'mldir' in args:
-    mldir = args['mldir']
-    if mldir != None and len(mldir)>0:
-      dict.update({'logdir': mldir[0]})
-  dict.update({dgbkeys.learntypedictstr: traintype.name})
-  return ret
+        outputnmfld.background = None
+    else:
+      outputnmfld.background = '#FF0000'
+    outputnmfld.value = new
+    if outputnmfld.background == curbg:
+      return
+    curdoc().clear()
+    curdoc().add_root(mainpanel)
 
-def doRun( cb = None ):
-  modelnm = outputnmfld.value
-  modtype = dgbmlio.getModelType( info ) 
-  canwrite = dgbmlio.modelNameIsFree(modelnm,modtype,args=args,reload=True)
-  if not canwrite:
-    odcommon.log_msg( 'Output model is not writable, please provide another name' )
-    #TODO: replace with netpacket
-    return False
+  def getParsGrp( platformnm ):
+    for platform,parsgroup in zip(ML_PLFS,parsgroups):
+      if platform[0] == platformnm:
+        return parsgroup['grp']
+    return None
+
+  def selParsGrp( platformnm ):
+    parsgrp = getParsGrp( platformnm )
+    if parsgrp == None:
+      return
+    curdoc().clear()
+    parameterspanel.child = column( parsgrp, parsbackbut )
+    curdoc().add_root(mainpanel)
+    this_service.sendObject('platform_change', platformnm)
+
+  def getUiParams():
+    parsgrp = getParsGrp( platformfld.value )
+    if platformfld.value == uikeras.getPlatformNm():
+      return uikeras.getUiParams( keraspars )
+    elif platformfld.value == uisklearn.getPlatformNm():
+      return uisklearn.getUiParams( sklearnpars )
+    return {}
+
+  def getProcArgs( platfmnm, pars, outnm ):
+    traintype = dgbmlapply.TrainType.New
+    ret = {
+      'posargs': [examplefilenm],
+      'odargs': odcommon.getODArgs( args ),
+      'dict': {
+        'platform': platfmnm,
+        'parameters': pars,
+        'output': outnm
+      }
+    }
+    dict = ret['dict']
+    if 'model' in args:
+      model = args['model']
+      if model != None and len(model)>0:
+        dict.update({'model': model[0]})
+        if args['transfer']:
+          traintype = dgbmlapply.TrainType.Transfer
+        else:
+          traintype = dgbmlapply.TrainType.Resume
+    if 'mldir' in args:
+      mldir = args['mldir']
+      if mldir != None and len(mldir)>0:
+        dict.update({'logdir': mldir[0]})
+    dict.update({dgbkeys.learntypedictstr: traintype.name})
+    return ret
+
+  def doRun( cb = None ):
+    modelnm = outputnmfld.value
+    modtype = dgbmlio.getModelType( info ) 
+    canwrite = dgbmlio.modelNameIsFree(modelnm,modtype,args=args,reload=True)
+    if not canwrite:
+      odcommon.log_msg( 'Output model is not writable, please provide another name' )
+      #TODO: replace with netpacket
+      return False
   
-  scriptargs = getProcArgs( platformfld.value, getUiParams(), \
+    scriptargs = getProcArgs( platformfld.value, getUiParams(), \
                             modelnm )
-  cmdtorun = getPythonCommand( trainscriptfp, scriptargs['posargs'], \
-                               scriptargs['dict'], scriptargs['odargs'] )
-  return execCommand( cmdtorun, background=True )
+    cmdtorun = getPythonCommand( trainscriptfp, scriptargs['posargs'], \
+                              scriptargs['dict'], scriptargs['odargs'] )
+    odcommon.log_msg( 'Starting process:', cmdtorun )
 
-def doAbort( proc ):
-  if isRunning(proc):
-    proc = kill( proc )
-  return None
+    this_service.sendEvent('training_started')
 
-def doPause( proc ):
-  pauseProcess( proc )
-  return proc
+    return execCommand( cmdtorun, background=True )
 
-def doResume( proc ):
-  resumeProcess( proc )
-  return proc
+  def doAbort( proc ):
+    if isRunning(proc):
+      proc = kill( proc )
+    return None
 
-def trainMonitorCB( proc ):
-  if proc == None or isRunning(proc):
+  def doPause( proc ):
+    pauseProcess( proc )
+    return proc
+
+  def doResume( proc ):
+    resumeProcess( proc )
+    return proc
+
+  def trainMonitorCB( proc ):
+    if proc == None or isRunning(proc):
+      return True
+    try:
+      stat = proc.status()
+    except psutil.NoSuchProcess:
+      if not odcommon.batchIsFinished( odcommon.get_log_file() ):
+        odcommon.log_msg( '\nProcess is no longer running (crashed or terminated).' )
+        odcommon.log_msg( 'See OpendTect log file for more details (if available).' )
+      else:
+        this_service.sendEvent('training_finished')
+      return False
     return True
-  try:
-    stat = proc.status()
-  except psutil.NoSuchProcess:
-    if not odcommon.batchIsFinished( odcommon.get_log_file() ):
-      odcommon.log_msg( '\nProcess is no longer running (crashed or terminated).' )
-      odcommon.log_msg( 'See OpendTect log file for more details (if available).' )
-    return False
-  return True
 
-platformfld.on_change('value',mlchgCB)
-outputnmfld.on_change('value_input',nameChgCB)
-buttonsgrp = uibokeh.getRunButtonsBar( doRun, doAbort, doPause, doResume, trainMonitorCB )
-trainpanel.child = column( platformfld, platformparsbut, outputnmfld, buttonsgrp )
+  platformfld.on_change('value',mlchgCB)
+  outputnmfld.on_change('value_input',nameChgCB)
+  buttonsgrp = uibokeh.getRunButtonsBar( doRun, doAbort, doPause, doResume, trainMonitorCB )
+  trainpanel.child = column( platformfld, platformparsbut, outputnmfld, buttonsgrp )
 
-def initWin():
-  mllearntype = info[dgbkeys.learntypedictstr]
-  if mllearntype == dgbkeys.loglogtypestr or \
-     mllearntype == dgbkeys.seisproptypestr:
-    platformfld.value = uisklearn.getPlatformNm(True)[0]
-  else:
-     platformfld.value = uikeras.getPlatformNm(True)[0]
-  mlchgCB( 'value', 0, platformfld.value )
-  nameChgCB( 'value', 0, dgbkeys.modelnm )
-  curdoc().title = 'Machine Learning'
+  def initWin():
+    mllearntype = info[dgbkeys.learntypedictstr]
+    if mllearntype == dgbkeys.loglogtypestr or \
+      mllearntype == dgbkeys.seisproptypestr:
+      platformfld.value = uisklearn.getPlatformNm(True)[0]
+    else:
+      platformfld.value = uikeras.getPlatformNm(True)[0]
+    mlchgCB( 'value', 0, platformfld.value )
+    nameChgCB( 'value', 0, dgbkeys.modelnm )
+    curdoc().title = 'Machine Learning'
 
-initWin()
+  initWin()
---
+++ servicemgr.py
@@ -0,0 +1,139 @@
+#
+# (C) dGB Beheer B.V.; (LICENSE) http://opendtect.org/OpendTect_license.txt
+# AUTHOR   : Wayne Mogg
+# DATE     : January 2020
+#
+# Service Manager
+#
+# 
+import json
+import os
+import psutil
+import signal
+import socket
+import struct
+import sys
+import threading
+import odpy.common as odcommon
+
+class ServiceMgr:
+  def __init__(self, cmdserver, ppid):
+    self.host = None
+    self.port = None
+    if '@' in cmdserver:
+      info = cmdserver.split('@')
+      cmdserver = info[1]
+    if ':' in cmdserver:
+      info = cmdserver.split(':')
+      self.host = info[0]
+      self.port = int(info[1])
+
+    self._parentproc = None
+    if ppid > 0:
+      self._parentproc = psutil.Process(ppid)
+      self._parentChkTimer = odcommon.Timer(15, self._parentChkCB)
+      self._parentChkTimer.start()
+
+    self._actions = dict()
+    
+  def __enter__(self):
+    return self
+  
+  def __exit__(self, exc_type, exc_value, traceback):
+    if self._parentproc != None:
+      self._parentChkTimer.cancel()
+    
+  def _parentChkCB(self):
+    if self_parentproc != None and not self._parentproc.is_running():
+      odcommon.std_msg('Found dead parent, exiting')
+      self.stop()
+      os.kill(psutil.Process().pid, signal.SIGINT)
+
+  def sendEvent(self, eventstr):
+    with Connection(self.host, self.port) as conn:
+      packet = Packet()
+      packet.setIsNewRequest()
+      action = {'action': eventstr}
+      packet.setTextPayload(action)
+      conn.sendPacket(packet)
+
+  def sendObject(self, objkey, jsonobj):
+    with Connection(self.host, self.port) as conn:
+      packet = Packet()
+      packet.setIsNewRequest()
+      obj = dict()
+      obj[objkey] = jsonobj
+      packet.setTextPayload(obj)
+      conn.sendPacket(packet)
+    
+class Connection:
+  def __init__(self, host, port):
+    self.addr = (host, port)
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.sock.setblocking(False)
+    self.sock.connect_ex(self.addr)
+    
+  def __enter__(self):
+    return self
+  
+  def __exit__(self, exc_type, exc_value, traceback):
+    self.sock.close()
+  
+  def sendPacket(self, packet):
+    pack = packet.packet
+    while len(pack):
+      try:
+        sent = self.sock.send(pack)
+      except BlockingIOError:
+        pass
+      else:
+        pack = pack[sent:]
+      
+
+class Packet:
+  _curreqid = 0
+  def __init__(self):
+    self._reqid = None
+    self._subid = None
+    self._jsonheader_len = None
+    self.jsonheader = None
+    self.packet = None
+    self._lock = threading.Lock()
+
+  def setIsNewRequest(self):
+    with self._lock:
+      self._curreqid += 1
+    self._reqid = self._curreqid
+    self._subid = -1
+    return self._reqid
+    
+  def setTextPayload(self, jsonobj):
+    content_encoding = 'utf-8'
+    payload = {
+          'content_bytes': self._json_encode(jsonobj, content_encoding),
+          'content_type': 'text/json',
+          'content_encoding': content_encoding,
+          'arrsize': None,
+    }
+    self._createPacket(payload)
+
+  def _createPacket(self, payload):
+    jsonheader = {
+        'byteorder': sys.byteorder,
+        'content-type': payload['content_type'],
+        'content-encoding': payload['content_encoding'],
+        'content-length': len(payload['content_bytes']),
+        }
+    if payload['arrsize'] != None:
+      jsonheader.update({ 'array-shape': payload['arrsize'] })
+
+    jsonheader_bytes = self._json_encode(jsonheader, 'utf-8')
+    payload_bytes = jsonheader_bytes + payload['content_bytes']
+    od_hdr = struct.pack('=i',len(payload_bytes)) \
+             + struct.pack('=i',self._reqid) \
+             + struct.pack('=h',self._subid)
+    self.packet = od_hdr + payload_bytes
+
+  def _json_encode(self, obj, encoding):
+    json_hdr = json.dumps(obj, ensure_ascii=False).encode(encoding)
+    return struct.pack('=i',len(json_hdr)) + json_hdr
