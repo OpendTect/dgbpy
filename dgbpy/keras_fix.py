ca8857f5d3ff9404883a8aa55924cb5f6ddf9b0f
Arnaud Huck <arnaud.huck@dgbes.com>
2021-11-10 14:16:46 +0100

    Updated keras code to make it compile with newer versions
    Making the keras/tensorflow code compatible with versions >2.2 <=2.7 (latest)

--- dgbpy/keras_fix.py
+++ dgbpy/keras_fix.py
@@ -1,5 +1,3 @@
-import tensorflow as tf
-import keras.backend.tensorflow_backend as tfback
 
 def _get_available_gpus():
   """Get a list of available gpu devices (formatted as strings).
@@ -9,9 +7,20 @@ def _get_available_gpus():
     # Returns
       A list of available GPU devices.
   """
+  import tensorflow as tf
+  try:
+    import keras.backend.tensorflow_backend as tfback
+  except ModuleNotFoundError:
+    return []
+
   if tfback._LOCAL_DEVICES is None:
     devices = tf.config.list_logical_devices()
     tfback._LOCAL_DEVICES = [x.name for x in devices]
   return [x for x in tfback._LOCAL_DEVICES if 'device:gpu' in x.lower()]
 
-tfback._get_available_gpus = _get_available_gpus 
+try:
+  import keras.backend.tensorflow_backend as tfback
+  tfback._get_available_gpus = _get_available_gpus()
+except:
+  pass
+
