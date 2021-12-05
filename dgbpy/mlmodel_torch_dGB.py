c247e08e367afe5ba2c57fae0cfa6066d202e44b
unknown <ibrahim.olawale13@gmail.com>
2021-12-05 14:54:31 +0100

    reduced model architecture

--- dgbpy/mlmodel_torch_dGB.py
+++ dgbpy/mlmodel_torch_dGB.py
@@ -77,29 +77,26 @@ def ResNet18(nroutputs, dim, nrattribs):
       BatchNorm = BatchNorm1d
 
     b0 = nn.Sequential(
-    Conv(in_channels = nrattribs, out_channels = 64, kernel_size = 3, stride = 1, padding = 1),
-    BatchNorm(num_features = 64),
+    Conv(in_channels = nrattribs, out_channels = 4, kernel_size = 3, stride = 1, padding = 1),
+    BatchNorm(num_features = 4),
     nn.ReLU())
 
-    b1 = nn.Sequential(*create_resnet_block(input_filters = 64, output_filters = 64, num_residuals = 2, first_block = True, ndims=dim))
-    b3 = nn.Sequential(*create_resnet_block(input_filters = 64, output_filters = 128, num_residuals = 2, first_block = True, ndims=dim))
-    b5 = nn.Sequential(*create_resnet_block(input_filters = 128, output_filters = 256, num_residuals = 2, first_block = True, ndims=dim))
-    b7 = nn.Sequential(*create_resnet_block(input_filters = 256, output_filters = 512, num_residuals = 2, first_block = True, ndims=dim))
+    b1 = nn.Sequential(*create_resnet_block(input_filters = 4, output_filters = 4, num_residuals = 1, first_block = True, ndims=dim))
     if dim==3:
       model = nn.Sequential(
-      b0, b1, b3, b5, b7,
+      b0, b1,
       nn.AdaptiveAvgPool2d(output_size = (1, 1)),
       nn.Flatten(),
-      nn.Linear(in_features = 1024, out_features = nroutputs))
+      nn.Linear(in_features = 36, out_features = nroutputs))
     elif dim==2:
       model = nn.Sequential(
-      b0, b1, b3, b5, b7,
+      b0, b1,
       nn.AdaptiveAvgPool2d(output_size = (1, 1)),
       nn.Flatten(),
-      nn.Linear(in_features = 512, out_features = nroutputs))
+      nn.Linear(in_features = 4, out_features = nroutputs))
     elif dim==1:
       model = nn.Sequential(
-      b0, b1, b3, b5, b7,
+      b0, b1,
       nn.AdaptiveAvgPool2d(output_size = (1, 1)),
       nn.Flatten(),
       nn.Linear(in_features = 1, out_features = nroutputs))
