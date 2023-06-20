f42a7cc40b58b1f37539b3bad92aed2cc418b235
waynegm <wayne.mogg@gmail.com>
2023-06-20 13:42:35 +1000

    Include Python backtrace in server startup exception reports
    Summary:
    Enhance server startup error reporting by including a backtrace from Python
    Changes ported from main branch
    
    Test Plan: Tested on Linux
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Differential Revision: https://phabricator.dgbes.com:8443/D16045

--- dgbpy/deeplearning_apply_serverlib.py
+++ dgbpy/deeplearning_apply_serverlib.py
@@ -396,6 +396,12 @@ class Message:
                  + struct.pack('=h',self._subid)
         return od_hdr + payload
 
+    def _make_exception_report(self, msg, exc):
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        stackstr = ''.join(tb.extract_tb(exc_tb,limit=10).format())
+        return f'{msg}:\n{repr(exc)} on line {str(exc_tb.tb_lineno)} of script {fname}\n{stackstr}\n\n{self.applier.debugstr}'
+
     def _create_response_json_content(self):
         action = self.request.get('action')
         content = { 'result': None }
@@ -410,7 +416,7 @@ class Message:
               self.applier.setOutputs( self.request.get('value') )
               content['result'] = 'Output names received'
             except Exception as e:
-              content = {"result": f'start error exception: {repr(e)}.'}
+              content = {"result": self._make_exception_report('Start error exception', e)}
         else:
             content['result'] = f'Error: invalid action "{action}".'
         content_encoding = 'utf-8'
@@ -432,10 +438,7 @@ class Message:
             else:
                 content = {"result": f'Error: invalid action "{action}".'}
         except Exception as e:
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            stackstr = ''.join(tb.extract_tb(exc_tb,limit=10).format())
-            content = {'result': f'Apply error exception:\n{repr(e)+" on line "+str(exc_tb.tb_lineno)+" of script "+fname}\n{stackstr}\n\n{self.applier.debugstr}'}
+            content = {'result': self._make_exception_report('Apply error exception', e)}
             self.applier.debugstr = ''
             content_encoding = 'utf-8'
             response = {
