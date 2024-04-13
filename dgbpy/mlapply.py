65835ecdd9b76af4f1887d0caafc6d5d63d8d84c
waynegm <wayne.mogg@gmail.com>
2024-04-13 14:02:04 +1000

    Fix OD5445 Support import of flexible size img2img ml models
    Summary:
    Fixes OD-5445 Fix various issues with apply of multi channel, flexible size
     keras models
    AdaptTo/FromModel function in dgbkeras is very complex so as not to break other workflows
    I put the correct logic for img2img in separate functions so all 2D/3D, flexible size options
    work.
    
    Test Plan: Tested on Linux
    
    Reviewers: arnaud
    
    Reviewed By: arnaud
    
    Differential Revision: https://phabricator.dgbes.com/D17079

--- dgbpy/mlapply.py
+++ dgbpy/mlapply.py
@@ -498,7 +498,7 @@ def doApply( model, info, samples, scaler=None, applyinfo=None, batchsize=None )
   if platform == dgbkeys.kerasplfnm:
     import dgbpy.dgbkeras as dgbkeras
     
-    res = dgbkeras.apply( model, samples, isclassification, withpred, withprobs, withconfidence, doprobabilities, \
+    res = dgbkeras.apply( model, info, samples, isclassification, withpred, withprobs, withconfidence, doprobabilities, \
                           dictinpshape, scaler=None, batch_size=batchsize  )
   elif platform == dgbkeys.scikitplfnm:
     import dgbpy.dgbscikit as dgbscikit
